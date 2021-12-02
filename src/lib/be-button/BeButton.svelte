9cb16c2451104d0fb566bf196f2cbadc97179660
beerui <beerui2021@gmail.com>
2021-12-02 09:46:21 +0800

    feat new branch

--- src/lib/be-button/BeButton.svelte
+++ src/lib/be-button/BeButton.svelte
@@ -1,15 +1,15 @@
 <script lang="ts">
     import { createEventDispatcher } from "svelte";
 
-    export let type: String = ''
-    export let size: String = ''
-    export let round: String = ''
-    export let disabled: Boolean = false
-    export let loading: Boolean = false
-    export let autofocus: Boolean = false
-    export let style: String = ''
-    export let buttonSize = ['', 'large', 'medium', 'small', 'mini'] as const
-    export let buttonNativeType = ['button', 'submit', 'reset'] as const
+    export let type = ''
+    export let size = ''
+    export let round = ''
+    export let disabled = false
+    export let loading = false
+    export const autofocus = false
+    export const style = ''
+    export const buttonSize = ['', 'large', 'medium', 'small', 'mini'] as const
+    export const buttonNativeType = ['button', 'submit', 'reset'] as const
 
     const dispatch = createEventDispatcher()
     const handleClick = (evt: MouseEvent) => {
@@ -20,12 +20,12 @@
 <button
     class={[
         'beer-button',
-        disabled ? 'beer-button--disabled' : '',
-        size ? 'beer-button--' + size : '',
-        type ? 'beer-button--' + type : '',
-        round ? 'beer-button--' + round : '',
-        loading ? 'beer-button--' + loading : ''
-    ].join(' ')}
+        disabled ? ' beer-button--disabled' : '',
+        size ? ' beer-button--' + size : '',
+        type ? ' beer-button--' + type : '',
+        round ? ' beer-button--' + round : '',
+        loading ? ' beer-button--' + loading : ''
+    ].join('')}
     autofocus
     style
     on:click={handleClick}
