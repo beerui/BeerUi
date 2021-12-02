de3158957a26dd3220d6d22d2e11f680cd5c2085
beerui <beerui2021@gmail.com>
2021-12-02 19:16:47 +0800

    merge

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,11 +1,6 @@
 <script>
-	import { First, BeButton, BeAlert } from '$lib';
+	import { BeAlert } from '$lib';
 </script>
-
-<First title="dwrew" name="2222" textColor="yellowgreen" />
-<BeButton size="small" type="primary" disabled>提交</BeButton>
-<a href='content/1'>content 1</a>
-<a href='content/2'>content 2</a>
 <BeAlert title="这是alert" type="success">
 	<!-- 这是描述 -->
 </BeAlert>
