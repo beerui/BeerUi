f658953b5f0ec770dbebc6ed8dd39e3204faecdf
beerui <beerui2021@gmail.com>
2021-12-03 18:51:34 +0800

    feat dialog page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,5 @@
 <script lang='ts'>
-	import { BeAlert } from '$lib';
 </script>
-<BeAlert title="这是alert" type="success">
-	<!-- 这是描述 -->
-</BeAlert>
 <h1>BeerUi 展板</h1>
 <ul>
 	<li><a href='button'>Button 示例</a></li>
