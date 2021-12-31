79ebab619b52ac1f427bea82c3827dea011c634f
叶陈华 <742436967@qq.com>
2021-12-31 16:31:50 +0800

    ADD:选择区划接口对接

--- src/routes/datePicker/index.svelte
+++ src/routes/datePicker/index.svelte
@@ -4,15 +4,11 @@
 </script>
 <div class='page-container'>
   <h2>datePicker</h2>
+  <h3>基本用法</h3>
   <p class='desc'>datePicker 日期选择</p>
-  <h3>基本用法</h3>
   <BeDatePicker value={'2021-10-11'} selectMode={"day"} format={"yyyy-MM-dd"}/>
-  <h2>datePicker</h2>
   <p class='desc'>datePicker 月份选择</p>
-  <h3>基本用法</h3>
   <BeDatePicker value={'2021-10'} selectMode={"month"} format={"yyyy-MM"}/>
-  <h2>datePicker</h2>
   <p class='desc'>datePicker 年份选择</p>
-  <h3>基本用法</h3>
   <BeDatePicker value={'2021'} selectMode={"year"} format={"yyyy"}/>
 </div>
