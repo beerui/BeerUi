c97659401eafa7e1b59526d27d7761e2c6e6a630
beerui <beerui2021@gmail.com>
2022-01-10 14:40:57 +0800

    feat add style pack

---
+++ src/lib/assets/scss/webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+	mode: 'development',
+	entry: './index.js',
+	output: {
+		filename: '[name].js',
+		path: path.resolve(__dirname, 'dist'),
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: [
+					// 将 CSS 转化成 CommonJS 模块
+					'css-loader',
+				],
+			},
+			{
+				test: /\.s[ac]ss$/i,
+				use: [
+					MiniCssExtractPlugin.loader,
+					{ loader: 'css-loader', options: { importLoaders: 1 } },
+					// 将 CSS 转化成 CommonJS 模块
+					'postcss-loader',
+					// 将 Sass 编译成 CSS
+					'sass-loader',
+				],
+			},
+		],
+	},
+	plugins: [
+		new MiniCssExtractPlugin({
+			filename: 'beer.css',
+		}),
+	],
+	devServer: {
+		contentBase: './',
+		host: '0.0.0.0',
+		compress: true,
+		port: 9000,
+		open: false,
+		writeToDisk: true,
+		stats: 'errors-only',
+	},
+};
