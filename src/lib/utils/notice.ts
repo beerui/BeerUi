9b929ede6e70acc06b9458d1f76b018c435d23c2
ysp <yangshuaipeng@jw.com>
2022-01-13 16:08:47 +0800

    feat:notice component onClose

--- src/lib/utils/notice.ts
+++ src/lib/utils/notice.ts
@@ -5,8 +5,12 @@
  interface options {
   title:string,
   message:string,
+  onClose?:() => void,
+  onClick?:() => void,
   duration?:number,
   position?:string
+  titleColor?:string
+  messageColor?:string
 }
 
 interface instanceType extends options {
@@ -15,6 +19,7 @@ interface instanceType extends options {
   timer:NodeJS.Timeout|null,
   verticalOffset:number,
   verticalProperty:string,
+  close?:() => void
 }
 
 export default class Notice{
@@ -24,33 +29,33 @@ export default class Notice{
   position:string; // 位置
   private _body:HTMLElement = document.body // 私有属性 获取body
   instances:Array<instanceType> = [];  // 储存弹窗信息
-  instance = <instanceType>{};
+  // instance = <instanceType>{};
   seed = 0;
   id:number;
-
+  titleColor:string
+  messageColor:string
   // constructor(){
   // }
 
-  setNotice(options:options):void {
+  setNotice(options:options):instanceType {
     this.title = options.title;
     this.message = options.message;
     this.position = options.position || 'top-right';
     this.duration = options.duration || 0
-    this.setHtml()
-  }
-
-  setHtml():void {
+    this.titleColor = options.titleColor
+    this.messageColor = options.messageColor
+    const userOnClose = options.onClose
+    
     const container:HTMLElement = document.createElement('div');
     container.classList.add('be-notify');
     container.classList.add(this.setPositionClass(this.position));
     container.innerHTML = `
     <div class="be-notify__group">
-      <h2 class="be-notify__title">${this.title}</h2>
-      <div class="be-notify__content">${this.message}</div>
+      <h2 class="be-notify__title" style="color:${this.titleColor?this.titleColor:'#000'}" >${this.title}</h2>
+      <div class="be-notify__content" style="color:${this.messageColor?this.messageColor:'#000'}" >${this.message}</div>
     </div>
     `;
     this._body.appendChild(container);
-    // container.classList.add('be-notify-fade');
     setTimeout(() => {
       container.style.transform = "translateX(0%)"
     });
@@ -60,6 +65,12 @@ export default class Notice{
     instance.dom = container
     instance.duration = this.duration
     instance.id = this.id
+    instance.onClose = ()=> {
+      this.close(instance.id,userOnClose)
+    }
+    instance.close = ()=> {
+      this.close(instance.id,userOnClose)
+    }
     instance.position = this.position
     instance.verticalProperty = this.setProperty(this.position)
     this.instances.push(instance);
@@ -77,12 +88,16 @@ export default class Notice{
     verticalOffset += 16;
     instance.verticalOffset = verticalOffset;
     instance.dom.style[this.setProperty(this.position)] = verticalOffset + 'px';
-    instance.timer = setTimeout(() => {
-      this.close(instance.id)
-    }, instance.duration);
+    if(instance.duration>0){
+      instance.timer = setTimeout(() => {
+        this.close(instance.id,userOnClose);
+      }, instance.duration);
+    }
+
+    return instance
   }
 
-  close(id:number):void {
+  close(id:number,userOnClose):void {
     let index = -1
     const len = this.instances.length
     const instance = this.instances.filter((instance,i)=>{
@@ -93,16 +108,26 @@ export default class Notice{
       return false;
     })[0]
     if(!instance) return;
+
+    if(typeof userOnClose === 'function'){
+      userOnClose(instance)
+    }
     const position = instance.position
     const removeHeight = instance.dom.offsetHeight
     instance.dom.parentNode.removeChild(instance.dom);
     this.instances.splice(index, 1);
     clearTimeout(instance.timer);
+    instance.timer = null
     if(len <= 1) return;
     for (let i = index; i < len - 1; i++) {
       if(this.instances[i].position === position){
         this.instances[i].dom.style[instance.verticalProperty] = parseInt(this.instances[i].dom.style[instance.verticalProperty],10) - removeHeight - 16 + 'px';
       }
+    }
+  }
+  closeAll(){
+    for (let i = this.instances.length - 1; i >=0; i--) {
+      this.instances[i].close()
     }
   }
     // 设置弹框类名
