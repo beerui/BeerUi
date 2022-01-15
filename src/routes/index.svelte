0a66b6fe7c4d1cdca053761d3f9362bf1020625c
杨帅鹏 <1262051766@qq.com>
2022-01-15 15:57:04 +0800

    feat BeeUI desc

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,13 @@
 <script lang='ts'>
 </script>
 <h2 class='title'>什么是BeerUi?</h2>
-<a class='desc' href='/components'>现在开始</a>
+<!-- <a class='desc' href='/components'>现在开始</a> -->
+<h3 class='desc'>这是一个Svelte UI 组件库</h3>
+<p class='content'>2021年11月19日晚,在路边摊此起彼伏的喧闹声中。"来!干杯",一口啤酒入喉,沁人心腑。玩笑中又透露出一丝的认真,叫BeerUI吧!在这一天一个Svelte UI 组件库BeerUI诞生了。</p>
+<p class='content'>就像它的名字Beer一样,我们致力于使用它时如啤酒般清爽,平顺甘醇,也流露出些许的不羁与放荡,回味时也有丝丝的香甜,让你欲罢不能流连忘返。当然这也是我们坚定不移的理念,在这充满荆棘且蜿蜒曲折的路上,以键盘为剑,我们握紧手中剑披荆斩棘走在这路上,通向崇高的目标的道路,总是崎岖的艰难的,也许会慢但是我们永远不会停止。</p>
+
 <style lang='scss'>
 	.title {color: var(--text-color-primary);text-align: center;margin: 30px 0;}
 	.desc {color: var(--text-color-primary);padding: 0 30px;text-align: center;}
+	.content{color: var(--text-color-primary);text-indent:50px;padding-left: 10px;padding-right: 10px;width: 800px; margin: 0 auto;line-height: 25px;}
 </style>
