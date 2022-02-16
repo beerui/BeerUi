94a52427faa68d67c35ff0947ab24cf67ace00bd
NiceSmall <motou2017@163.com>
2022-02-16 22:34:29 +0800

    fixed 发布包和自动部署报错1

--- src/routes/components/datePicker/index.svelte
+++ src/routes/components/datePicker/index.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
   import { BeDatePicker } from '$lib';
-  import { BeTimePicker } from '$lib';
 
   let value = ''
   $:{
@@ -23,5 +22,4 @@
   <p class='desc'>datePicker 年份选择</p>
   <BeDatePicker value={'2021'} selectMode={"year"} format={"yyyy"} />
   <p class='desc'>timePicker 时间选择</p>
-  <BeTimePicker />
 </div>
