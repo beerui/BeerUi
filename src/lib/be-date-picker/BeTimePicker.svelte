ad0e1c2bc7a2adc5204acec76eff0abf4a4f6474
beerui <beerui2021@gmail.com>
2022-02-17 09:13:46 +0800

    test 发布包和自动部署报错

--- src/lib/be-date-picker/BeTimePicker.svelte
+++ src/lib/be-date-picker/BeTimePicker.svelte
@@ -1,24 +1,24 @@
 <script lang='ts'>
-	// import Time from './panel/time.svelte';
-	// import BeInput from '../be-input/BeInput.svelte';
-	// import clickOutside from '$lib/_actions/clickOutside';
-	// export let value;
-	// let visible;
-	// function handleShowDatePopper(e) {
-	// 	visible = true;
-	// 	console.log('handleShowDatePopper');
-	//
-	// }
-	//
-	// function handleChange() {
-	// 	visible = false;
-	// }
-	// function handleCloseDatePopper(v) {
-	// 	visible = false;
-	// }
+	import Time from './panel/time.svelte';
+	import BeInput from '../be-input/BeInput.svelte';
+	import clickOutside from '$lib/_actions/clickOutside';
+	export let value;
+	let visible;
+	function handleShowDatePopper(e) {
+		visible = true;
+		console.log('handleShowDatePopper');
+
+	}
+
+	function handleChange() {
+		visible = false;
+	}
+	function handleCloseDatePopper(v) {
+		visible = false;
+	}
 //	发布包和自动部署报错
 </script>
-<!--<div class='be-date' use:clickOutside={{ cb: handleCloseDatePopper }} on:outside={handleCloseDatePopper}>-->
-<!--	<BeInput on:change={handleChange} on:focus={handleShowDatePopper} />-->
-<!--	<Time bind:visible={visible} />-->
-<!--</div>-->
+<div class='be-date' use:clickOutside={{ cb: handleCloseDatePopper }} on:outside={handleCloseDatePopper}>
+	<BeInput on:change={handleChange} on:focus={handleShowDatePopper} />
+	<Time bind:visible={visible} />
+</div>
