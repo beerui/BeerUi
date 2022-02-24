a9d513b410db006cb84115497c0e9d8ab1cda49f
叶陈华 <742436967@qq.com>
2022-02-24 13:41:44 +0800

    RESET：BeSelect

--- src/lib/be-select/BeOption.svelte
+++ src/lib/be-select/BeOption.svelte
@@ -1,6 +1,16 @@
 <script lang="ts">
-export let options;
+// import { createEventDispatcher } from "svelte";
+import { BeerPS } from "$lib/utils/beerui";
+
+  // const dispath = createEventDispatcher()
+  const selectChange = (key) => BeerPS.publish('selectChange', key)
+  export let label;
+	export let value
+  const select = () => {
+    console.log(label, value);
+    
+    selectChange({label, value})
+    // dispath('change', value)
+  }
 </script>
-{#each options as item, index}
-<option value={item.value}>{item.label}</option>
-{/each}
+ <li class='be-select-dropdown__item' on:click={select}>{label}</li>
