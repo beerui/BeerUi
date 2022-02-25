80625ff754d8fc0a64846a80d2db370be12a40f2
叶陈华 <742436967@qq.com>
2022-02-25 08:45:50 +0800

    FIXED：be-select

--- src/lib/be-select/BeSelect.svelte
+++ src/lib/be-select/BeSelect.svelte
@@ -1,16 +1,18 @@
 <script lang='ts'>
 	import BeIcon from './../be-icon/BeIcon.svelte';
 	import BeInput from '../be-input/BeInput.svelte';
-	import { cubicInOut, sineIn } from 'svelte/easing';
-	import { createEventDispatcher } from 'svelte'
+	import { cubicInOut } from 'svelte/easing';
+	import { createEventDispatcher, getContext, setContext } from 'svelte'
 	import clickOutside from '$lib/_actions/clickOutside';
-	import { BeerPS } from "$lib/utils/beerui";
+	import { BeerPS, genKey } from "$lib/utils/beerui";
+	import SelectStore from './select'
 	let dispatch = createEventDispatcher()
-
-	// export let value = '1';
 	export let options;
 	// 下拉框选中的值
-	export let value;
+	export let value;	
+	// const selectStore = new SelectStore({value})
+	// setContext('selectStore', selectStore)
+	let inputValue = getContext('lable')
 	// 是否禁用
 	export let disabled = false;
 	// 位置
@@ -19,14 +21,26 @@
 	let visible = false;
 	// 获取输入框
 	let input
+	const key = `selectChange_${ genKey() }` 
 
-	BeerPS.subscribe('selectChange', data => {
-		console.log('---', data);
-		value = data.label
-		handleClosePopper()
-		dispatch('change', data.value)
+	setContext('selectChangeKey', key)
+
+	$: {
+		setContext('selectValue', value)
+	}
+
+	setTimeout(() => {
+		BeerPS.publish('test','11231323')
+	}, 1010);
+
+	BeerPS.subscribe(key, items => {
+		console.log(items);
+		visible = false
+		value = items.value
+		inputValue = items.label
 	})
-	
+
+
 
 	function handleShowPopper() {
 			visible = true;
@@ -34,19 +48,8 @@
 	function handleClosePopper(){
 			visible = false
 	}
-	// 箭头动画
-	function arrowsRotate(node, params) {
-		return {
-			duration: 300,
-			css: (t) => {
-				const eased = sineIn(t);
-				return `transform: rotate(${ eased * (visible ? -45 : 135) }deg);`;
-			}
-		};
-	}
 	const change = (e) => {
 		console.log(e);
-		
 	}
 	function zoomIn(node, params) {
 		return {
@@ -64,14 +67,14 @@
 
 <div class='be-select' use:clickOutside={{ cb: handleClosePopper }}>
 	<!-- <div class={['be-select__content',disabled ? ' is-disabled':''].join('')}> -->
-		<BeInput on:focus={handleShowPopper} bind:value bind:this={input} readonly disabled={disabled}>
+		<BeInput on:focus={handleShowPopper} value={inputValue} bind:this={input} readonly disabled={disabled}>
 			<div slot='suffix' class="input-suffix-icon" class:is-reverse = {visible}>
 				<BeIcon name='chevron-down' width='20' height='20' />
 			</div>
 		</BeInput>
 	<!-- </div> -->
 	{#if visible}
-		<div class='be-select__option' in:zoomIn="{{duration: 300}}" out:zoomIn="{{duration: 300}}">
+		<div class='be-select__option' in:zoomIn="{{duration: 300}}" out:zoomIn="{{duration: 200}}">
 			<ul class={['be-select__option_content',position === 'top'?' is_top':''].join('')}>
 				<slot></slot>
 			</ul>
