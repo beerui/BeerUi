70c2824cf363ac675e270b8200dfae9fe5cc4b16
叶陈华 <742436967@qq.com>
2022-03-02 11:33:21 +0800

    ADD：datePicker add icon

--- src/routes/components/datePicker/index.svelte
+++ src/routes/components/datePicker/index.svelte
@@ -20,13 +20,15 @@
   <p class='desc'>datePicker 日期选择</p>
   <BeDatePicker value={value} selectMode={"date"} format={"yyyy-MM-dd"} valueFormat={"yyyy/MM/dd"} disabledDate={disabledDate} on:change={changeTime}/>
   <p class='desc'>datePicker 日期范围选择</p>
-  <BeDatePicker selectMode={"daterange"} value={['2022-03-01','2022-03-03']} format={"yyyy-MM-dd"} valueFormat={"yyyy/MM/dd"} />
+  <BeDatePicker selectMode={"daterange"} value={['2022-03-01','2022-03-03']} format={"yyyy-MM-dd"} valueFormat={"yyyy/MM/dd"} disabledDate={disabledDate}/>
   <p class='desc'>datePicker 日期时间选择</p>
   <BeDatePicker value={''} selectMode={"datetime"} format={"yyyy-MM-dd HH:mm"} on:change={changeTime}/>
   <p class='desc'>datePicker 月份选择</p>
   <BeDatePicker value={'2021-12'} selectMode={"month"} format={"yyyy-MM"}/>
+  <p class='desc'>datePicker 月份范围选择</p>
+  <BeDatePicker selectMode={"monthrange"} format={"yyyy-MM"} on:change={changeTime}/>
   <p class='desc'>datePicker 年份选择</p>
   <BeDatePicker value={'2021'} selectMode={"year"} format={"yyyy"} />
   <p class='desc'>timePicker 时间选择</p>
-  <BeTimePicker value={(new Date())} format={"HH:mm"} {selectableRange} on:change={changeTime}/>
+  <BeTimePicker value='' format={"HH:mm"} {selectableRange} on:change={changeTime}/>
 </div>
