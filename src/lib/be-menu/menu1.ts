f5402c7aee7ae77de5cd5373b647bed450ebe55f
NiceSmall <motou2017@163.com>
2022-03-03 22:31:20 +0800

    up menu

--- src/lib/be-menu/menu1.ts
+++ src/lib/be-menu/menu1.ts
@@ -1,6 +1,5 @@
 import { genKey, hasClass } from '$lib/utils/beerui';
 
-
 export default class MenuStore {
 	private readonly dispatch: <EventKey extends Extract<keyof any, string>>(type: EventKey, detail?: any) => void;
 	subscribe: Function[] = []; // 订阅方法
@@ -12,7 +11,7 @@ export default class MenuStore {
 	public root: Node;
 
 	constructor(options) {
-		for (let option in options) {
+		for (const option in options) {
 			if (options.hasOwnProperty(option)) {
 				this[option] = options[option];
 			}
@@ -21,34 +20,40 @@ export default class MenuStore {
 		this.root = new Node({
 			level: 0,
 			store: this,
-			parent: null
+			parent: null,
+			pid: null
 		});
 	}
 
 	initTree(els) {
-		console.dir(els);
 		if (!els) return
 		const key = genKey()
 		this.root.key = key
 		this.convertToMap(els, this.root)
-		this.convertToTree(this.root, this.nodesMap, key, [])
+		this.convertToTree(this.root, key, [])
 		console.log('this.root', this.root);
 	}
 
 	// 转为树
-	convertToTree(tree, list, pkey, keys) {
+	convertToTree(tree, pkey, keys) {
 		keys.push(pkey)
 		const result = [];
-		for (const key in list) {
-			if (list[key].pkey === pkey) {
-				const item = list[key]
-				// const node = new Node({ ...item, level: item.level || 0, store: this })
-				result.push(item);
+		for (const key in this.nodesMap) {
+			if (this.nodesMap[key].pkey === pkey) {
+				const _node = new Node({
+					...this.nodesMap[key],
+					store: this.root.store
+				})
+				result.push(_node);
 			}
 		}
+		console.log('tree', tree);
+		console.log('result', result);
 		tree['children'] = result
-		for (const key in list) {
-			if (!keys.includes(list[key].key)) this.convertToTree(list[key], list, list[key].key, keys)
+		for (const key in this.nodesMap) {
+			if (!keys.includes(this.nodesMap[key].key)) {
+				this.convertToTree(this.nodesMap[key], this.nodesMap[key].key, keys);
+			}
 		}
 	}
 	// 清洗数据
@@ -57,14 +62,12 @@ export default class MenuStore {
 			const _item = el[1];
 			const key = this.getAttr(_item, 'key')
 			const index = this.getAttr(_item, 'index')
-			// const level = this.getAttr(_item, 'level')
 			const child = {
 				key: key || parent.key,
-				index,
 				level: parent.level,
+				index,
 				pkey: parent.key
 			}
-			if (key) child['level'] = ++child.level
 			if (_item && _item.children.length > 0) {
 				this.convertToMap(_item, child)
 			}
@@ -119,25 +122,27 @@ class Node {
 	private parent: any;
 	private id: number;
 	private level: any;
-	private store: any;
+	store: any;
 	key: string;
+	pid: string;
+	private pNode: any;
 	constructor(options) {
 		this.id = nodeIdSeed++;
 		this.level = 0;
-		for (let name in options) {
+		for (const name in options) {
 			if (options.hasOwnProperty(name)) {
 				this[name] = options[name];
 			}
 		}
 
-		if (this.parent) {
-			this.level = this.parent.level + 1;
+		if (this.pNode) {
+			this.level = this.pNode.level + 1;
 		}
 
-		// const store = this.store;
-		// if (!store) {
-		// 	throw new Error('[Node]store is required!');
-		// }
-		// store.registerNode(this);
+		const store = this.store;
+		if (!store) {
+			throw new Error('[Node]store is required!');
+		}
+		store.registerNode(this);
 	}
 }
