620e0d5199e4657d18d73995bd2bb9af63143239
beerui <beerui2021@gmail.com>
2022-03-10 10:07:15 +0800

    feat notice type tips

--- src/lib/utils/notice.ts
+++ src/lib/utils/notice.ts
@@ -1,158 +1,185 @@
 /**
  * this is notice component
  */
- interface options {
-  title:string,
-  message:string,
-  onClose?:() => void,
-  onClick?:() => void,
-  duration?:number,
-  position?:string
-  titleColor?:string
-  messageColor?:string
+interface options {
+	type: string;
+	title: string,
+	message: string,
+	onClose?: () => void,
+	onClick?: () => void,
+	duration?: number,
+	position?: string
+	titleColor?: string
+	messageColor?: string
 }
 
 interface instanceType extends options {
-  id:number,
-  dom:HTMLElement,
-  timer:NodeJS.Timeout|null,
-  verticalOffset:number,
-  verticalProperty:string,
-  close?:() => void
+	id: number,
+	dom: HTMLElement,
+	timer: NodeJS.Timeout | null,
+	verticalOffset: number,
+	verticalProperty: string,
+	close?: () => void
 }
 
-export default class Notice{
-  title: string; // 标题
-  message: string; // 内容
-  duration:number; // 间隔
-  position:string; // 位置
-  private _body:HTMLElement = document.body // 私有属性 获取body
-  instances:Array<instanceType> = [];  // 储存弹窗信息
-  // instance = <instanceType>{};
-  seed = 0;
-  id:number;
-  titleColor:string
-  messageColor:string
-  // constructor(){
-  // }
+export default class Notice {
+	title: string; // 标题
+	message: string; // 内容
+	duration: number; // 间隔
+	position: string; // 位置
+	private _body: HTMLElement = document.body; // 私有属性 获取body
+	instances: Array<instanceType> = [];  // 储存弹窗信息
+	// instance = <instanceType>{};
+	seed = 0;
+	id: number;
+	titleColor: string;
+	messageColor: string;
+	// constructor(){
+	// }
 
-  // 生成弹窗
-  setNotice(options:options):instanceType {
-    this.title = options.title;
-    this.message = options.message;
-    this.position = options.position || 'top-right';
-    this.duration = options.duration || 0
-    this.titleColor = options.titleColor
-    this.messageColor = options.messageColor
-    const userOnClose = options.onClose
-    const userOnclick = options.onClick
+	// 生成弹窗
+	private type: string;
+	setNotice(options: options): instanceType {
+		this.title = options.title;
+		this.message = options.message;
+		this.type = options.type;
+		this.position = options.position || 'top-right';
+		this.duration = options.duration || 0;
+		this.titleColor = options.titleColor;
+		this.messageColor = options.messageColor;
+		const userOnClose = options.onClose;
+		const userOnClick = options.onClick;
 
 
-    const container:HTMLElement = document.createElement('div');
-    container.classList.add('be-notify');
-    container.classList.add(this.setPositionClass(this.position));
-    container.innerHTML = `
-    <div class="be-notify__group">
-      <h2 class="be-notify__title" style="color:${this.titleColor?this.titleColor:'#000'}" >${this.title}</h2>
-      <div class='be-notice__close' id="notice-close" style='width: 16px;height: 16px;'>
-        <svg class="be-svg" width="100%" height="100%" viewBox="0 0 16 16" style='color:#909399' xmlns="http://www.w3.org/2000/svg">
-        <path d="M8.00005 8.91923L11.076 11.9951L11.9952 11.0759L8.91929 7.99999L11.9952 4.92409L11.076 4.00485L8.00005 7.08075L4.92412 4.00482L4.00488 4.92406L7.08081 7.99999L4.00488 11.0759L4.92412 11.9952L8.00005 8.91923Z" fill="currentColor" fill-opacity="0.9"/>
-        </svg>
-      </div>
-      <div class="be-notify__content" style="color:${this.messageColor?this.messageColor:'#000'}" >${this.message}</div>
-    </div>
-    `;
-    this._body.appendChild(container);
-    setTimeout(() => {
-      container.style.transform = "translateX(0%)"
-    });
+		const container: HTMLElement = document.createElement('div');
+		container.classList.add('be-notify');
+		container.classList.add(this.setPositionClass(this.position));
+		container.innerHTML = `
+		${this.computedType()}
+		<div class='be-notify__group'>
+		  <h2 class='be-notify__title' style='color:${ this.titleColor ? this.titleColor : '#000' }' >${ this.title }</h2>
+		  <div class='be-notice__close' id='notice-close' style='width: 16px;height: 16px;'>
+			<svg class='be-svg' width='100%' height='100%' viewBox='0 0 16 16' style='color:#909399' xmlns='http://www.w3.org/2000/svg'>
+				<path d='M8.00005 8.91923L11.076 11.9951L11.9952 11.0759L8.91929 7.99999L11.9952 4.92409L11.076 4.00485L8.00005 7.08075L4.92412 4.00482L4.00488 4.92406L7.08081 7.99999L4.00488 11.0759L4.92412 11.9952L8.00005 8.91923Z' fill='currentColor' fill-opacity='0.9'/>
+			</svg>
+		  </div>
+		  <div class='be-notify__content' style='color:${ this.messageColor ? this.messageColor : '#000' }' >${ this.message }</div>
+		</div>
+		`;
+		this._body.appendChild(container);
+		setTimeout(() => {
+			container.style.transform = 'translateX(0%)';
+		});
 
-    const instance=<instanceType>{};
-    this.id = this.seed ++
-    instance.dom = container
-    instance.duration = this.duration
-    instance.id = this.id
-    // 关闭回调
-    instance.onClose = ()=> {
-      this.close(instance.id,userOnClose)
-    }
-    // 关闭事件
-    instance.close = ()=> {
-      this.close(instance.id,userOnClose)
-    }
-    // 点击关闭按钮
-    const closeBtn = document.getElementById('notice-close')
-    closeBtn.addEventListener("click",  instance.close)
-    if(typeof userOnclick === 'function'){
-      instance.dom.addEventListener("click",userOnclick)
-      instance.onClick = ()=>{userOnclick}
-    }
+		const instance = <instanceType>{};
+		this.id = this.seed++;
+		instance.dom = container;
+		instance.duration = this.duration;
+		instance.id = this.id;
+		// 关闭回调
+		instance.onClose = () => {
+			this.close(instance.id, userOnClose);
+		};
+		// 关闭事件
+		instance.close = () => {
+			this.close(instance.id, userOnClose);
+		};
+		// 点击关闭按钮
+		const closeBtn = document.getElementById('notice-close');
+		closeBtn.addEventListener('click', instance.close);
+		if (typeof userOnClick === 'function') {
+			instance.dom.addEventListener('click', userOnClick);
+			instance.onClick = () => {
+				userOnClick;
+			};
+		}
 
-    instance.position = this.position
-    instance.verticalProperty = this.setProperty(this.position)
-    this.instances.push(instance);
-    let verticalOffset = 0;
+		instance.position = this.position;
+		instance.verticalProperty = this.setProperty(this.position);
+		this.instances.push(instance);
+		let verticalOffset = 0;
 
-      // 将同一位置的弹框过滤到一个数组中并设置偏移量
-    this.instances.filter(item => item.position === this.position).forEach((item,index) =>{
-      item.dom.style['z-index'] = 2000 + index
-      if(index !==0 ){
-        verticalOffset += item.dom.offsetHeight + 16
-      }else{
-        verticalOffset = 0
-      }
-    })
-    verticalOffset += 16;
-    instance.verticalOffset = verticalOffset;
-    instance.dom.style[this.setProperty(this.position)] = verticalOffset + 'px';
-    if(instance.duration>0){
-      instance.timer = setTimeout(() => {
-        this.close(instance.id,userOnClose);
-      }, instance.duration);
-    }
+		// 将同一位置的弹框过滤到一个数组中并设置偏移量
+		this.instances.filter(item => item.position === this.position).forEach((item, index) => {
+			item.dom.style['z-index'] = 2000 + index;
+			if (index !== 0) {
+				verticalOffset += item.dom.offsetHeight + 16;
+			} else {
+				verticalOffset = 0;
+			}
+		});
+		verticalOffset += 16;
+		instance.verticalOffset = verticalOffset;
+		instance.dom.style[this.setProperty(this.position)] = verticalOffset + 'px';
+		if (instance.duration > 0) {
+			instance.timer = setTimeout(() => {
+				this.close(instance.id, userOnClose);
+			}, instance.duration);
+		}
 
-    return instance
-  }
-  // 关闭
-  close(id:number,userOnClose):void {
-    let index = -1
-    const len = this.instances.length
-    const instance = this.instances.filter((instance,i)=>{
-      if (instance.id === id) {
-        index = i;
-        return true;
-      }
-      return false;
-    })[0]
-    if(!instance) return;
+		return instance;
+	}
+	computedType() {
+		if (this.type) {
+			const types = {
+				success: '<path d="M8 15C11.866 15 15 11.866 15 8C15 4.13401 11.866 1 8 1C4.13401 1 1 4.13401 1 8C1 11.866 4.13401 15 8 15ZM4.5 8.2065L5.2065 7.5L7 9.293L10.7925 5.5L11.5 6.2075L7 10.707L4.5 8.2065Z" fill="#67c23a" fill-opacity="0.9"/>',
+				warning: '<path d="M15 8C15 4.13401 11.866 1 8 1C4.13401 0.999998 1 4.134 1 8C0.999998 11.866 4.134 15 8 15C11.866 15 15 11.866 15 8ZM8.5 4.0003V9.49999H7.5V4.0003H8.5ZM7.39429 11H8.59429V12.2H7.39429V11Z" fill="#e6a23c" fill-opacity="0.9"/>',
+				info: '<path d="M8 15C11.866 15 15 11.866 15 8C15 4.13401 11.866 1 8 1C4.13401 1 1 4.13401 1 8C1 11.866 4.13401 15 8 15ZM7.3999 4H8.59985V5.19995H7.3999V4ZM7.50562 6.5H8.50562V11.9998H7.50562V6.5Z" fill="#909399" fill-opacity="0.9"/>',
+				error: '<path d="M15 8C15 4.13401 11.866 1 8 1C4.13401 0.999998 1 4.134 1 8C0.999998 11.866 4.134 15 8 15C11.866 15 15 11.866 15 8ZM5.67091 4.94754L8.00001 7.29075L10.3291 4.94754L11.0384 5.65251L8.70499 8L11.0383 10.3475L10.3291 11.0525L8.00001 8.70925L5.67092 11.0525L4.96168 10.3475L7.29503 8L4.96167 5.65251L5.67091 4.94754Z" fill="#f56c6c" fill-opacity="0.9"/>'
+			}
+			return `
+			<div class='be-icon' style='width: 24px;height: 24px;'>
+				<svg class="be-svg" width="100%" height="100%" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+					${types[this.type]}
+				</svg>
+			</div>
+			`
+		}
+		return ''
+	}
+	// 关闭
+	close(id: number, userOnClose): void {
+		let index = -1;
+		const len = this.instances.length;
+		const instance = this.instances.filter((instance, i) => {
+			if (instance.id === id) {
+				index = i;
+				return true;
+			}
+			return false;
+		})[0];
+		if (!instance) return;
 
-    if(typeof userOnClose === 'function'){
-      userOnClose(instance)
-    }
-    const position = instance.position
-    const removeHeight = instance.dom.offsetHeight
-    instance.dom.parentNode.removeChild(instance.dom);
-    this.instances.splice(index, 1);
-    clearTimeout(instance.timer);
-    instance.timer = null
-    if(len <= 1) return;
-    for (let i = index; i < len - 1; i++) {
-      if(this.instances[i].position === position){
-        this.instances[i].dom.style[instance.verticalProperty] = parseInt(this.instances[i].dom.style[instance.verticalProperty],10) - removeHeight - 16 + 'px';
-      }
-    }
-  }
-  closeAll(){
-    for (let i = this.instances.length - 1; i >=0; i--) {
-      this.instances[i].close()
-    }
-  }
-    // 设置弹框类名
-  setPositionClass(position:string):string{
-    return position.includes('right') ? 'right':'left'
-  }
-  // 设置弹框样式属性
-  setProperty(position:string):string {
-    return /^top-/.test(position) ? 'top' : 'bottom'
-  }
+		if (typeof userOnClose === 'function') {
+			userOnClose(instance);
+		}
+		const position = instance.position;
+		const removeHeight = instance.dom.offsetHeight;
+		instance.dom.parentNode.removeChild(instance.dom);
+		this.instances.splice(index, 1);
+		clearTimeout(instance.timer);
+		instance.timer = null;
+		if (len <= 1) return;
+		for (let i = index; i < len - 1; i++) {
+			if (this.instances[i].position === position) {
+				this.instances[i].dom.style[instance.verticalProperty] = parseInt(this.instances[i].dom.style[instance.verticalProperty], 10) - removeHeight - 16 + 'px';
+			}
+		}
+	}
+
+	closeAll() {
+		for (let i = this.instances.length - 1; i >= 0; i--) {
+			this.instances[i].close();
+		}
+	}
+
+	// 设置弹框类名
+	setPositionClass(position: string): string {
+		return position.includes('right') ? 'right' : 'left';
+	}
+
+	// 设置弹框样式属性
+	setProperty(position: string): string {
+		return /^top-/.test(position) ? 'top' : 'bottom';
+	}
 }
--- src/routes/components/notice/index.svelte
+++ src/routes/components/notice/index.svelte
@@ -1,136 +1,215 @@
-<script lang="ts" >
-import Notice from '$lib/utils/notice'
-import { onMount } from 'svelte'
- 
-import { BeButton } from '$lib';
-let notice;
-onMount(() => {
-  notice = new Notice()
-})
+<script lang='ts'>
+	import Notice from '$lib/utils/notice';
+	import { onMount } from 'svelte';
 
-function openInfo():void{
- notice.setNotice({
-    title: '相思',
-    message: '红豆生南国，春来发几枝',
-    duration:3000,
-  })
-}
+	import { BeButton } from '$lib';
+	import BeIcon from '$lib/be-icon/BeIcon.svelte';
+	import DemoBlock from '$lib/demo/DemoBlock.svelte';
 
-function openPosInfo(position:string):void{
-  let title:string = '提示'
-  let message:string = '这是信息'
-  if(position === 'top-right'){
-    title = '提示'
-    message = '两情若是久长时，又岂在朝朝暮暮。'
-  }else if(position === 'top-left'){
-    title = '江行寄远'
-    message = '思君不可得，愁见江水碧。'
-  }else if(position === 'bottom-right'){
-    title = '长恨歌'
-    message = '后宫佳丽三千人，三千宠爱在一身。'
-  }else if(position === 'bottom-left'){
-    title = '长恨歌'
-    message = '在天愿作比翼鸟，在地愿为连理枝。天长地久有时尽，此恨绵绵无绝期。'
-  }
-  notice.setNotice({
-    title,
-    message,
-    position,
-    duration:3000
-  })
-}
+	let notice;
+	onMount(() => {
+		notice = new Notice();
+	});
 
-function openColorNotice(){
-  notice.setNotice({
-    title: '上邪',
-    message: '山无陵，江水为竭，冬雷震震，夏雨雪，天地合，乃敢与君绝。',
-    duration:3000,
-    titleColor:"#2b9939",
-    messageColor:"#7376f0"
-  })
-}
+	function openInfo(): void {
+		notice.setNotice({
+			title: '相思',
+			message: '红豆生南国，春来发几枝',
+			duration: 3000
+		});
+	}
+
+	function openPosInfo(position: string): void {
+		let title: string = '提示';
+		let message: string = '这是信息';
+		if (position === 'top-right') {
+			title = '提示';
+			message = '两情若是久长时，又岂在朝朝暮暮。';
+		} else if (position === 'top-left') {
+			title = '江行寄远';
+			message = '思君不可得，愁见江水碧。';
+		} else if (position === 'bottom-right') {
+			title = '长恨歌';
+			message = '后宫佳丽三千人，三千宠爱在一身。';
+		} else if (position === 'bottom-left') {
+			title = '长恨歌';
+			message = '在天愿作比翼鸟，在地愿为连理枝。天长地久有时尽，此恨绵绵无绝期。';
+		}
+		notice.setNotice({
+			title,
+			message,
+			position,
+			duration: 3000
+		});
+	}
+
+	function openColorNotice() {
+		notice.setNotice({
+			title: '上邪',
+			message: '山无陵，江水为竭，冬雷震震，夏雨雪，天地合，乃敢与君绝。',
+			duration: 3000,
+			titleColor: '#2b9939',
+			messageColor: '#7376f0'
+		});
+	}
 
 
-// 
-let closeNotice;
-function openNotice(){
-closeNotice =  notice.setNotice({
-    title: '滕王阁序',
-    message: '落霞与孤鹜齐飞,秋水共长天一色',
-    duration:0,
-  })
-}
-function closeOne(){
-  closeNotice.close()
-}
+	//
+	let closeNotice;
 
-function closeAll(){
-  notice.closeAll()
-}
+	function openNotice() {
+		closeNotice = notice.setNotice({
+			title: '滕王阁序',
+			message: '落霞与孤鹜齐飞,秋水共长天一色',
+			duration: 0
+		});
+	}
 
-function openNotice1(){
-  notice.setNotice({
-    title: '三五七言',
-    message: '入我相思门',
-    duration:3000,
-    onClose:onClose
-  })
-}
-function onClose(){
-  alert('知我相思苦')
-}
+	function closeOne() {
+		closeNotice.close();
+	}
 
-function openNotice2(){
-  notice.setNotice({
-    title: '木兰花·拟古决绝词柬友',
-    message: '人生若只如初见，何事秋风悲画扇。',
-    duration:0,
-    onClick: onClick
-  })
-}
-function onClick(e){
-  // console.log(e)
-  alert('你好Beer UI')
-}
+	function closeAll() {
+		notice.closeAll();
+	}
 
+	function openNotice1() {
+		notice.setNotice({
+			title: '三五七言',
+			message: '入我相思门',
+			duration: 3000,
+			onClose: onClose
+		});
+	}
+
+	function onClose() {
+		alert('知我相思苦');
+	}
+
+	function openNotice2() {
+		notice.setNotice({
+			title: '木兰花·拟古决绝词柬友',
+			message: '人生若只如初见，何事秋风悲画扇。',
+			duration: 0,
+			onClick: onClick
+		});
+	}
+
+	function onClick(e) {
+		// console.log(e)
+		alert('你好Beer UI');
+	}
+
+	const openTypeNotice = (type) => {
+		notice.setNotice({
+			title: '提示',
+			message: type,
+			duration: 3000,
+			type
+		});
+	}
 </script>
 
-<div class="page-container content">
+<div class='page-container content'>
 	<h2>notice</h2>
 	<p>显示全局的通知提醒消息</p>
 	<h3>基本用法</h3>
-  <div class=" demo-notice">
-    <BeButton size="normal" type="default" on:click={openInfo}>点击弹出消息</BeButton>
-  </div>
-  <h3>指定位置</h3>
-  <p>默认为<code>top-right</code>可选择<code>bottom-right</code><code>top-left</code><code>bottom-left</code></p>
-  <div class=" demo-notice">
-    <BeButton size="normal" type="default" on:click={() =>openPosInfo('top-right')}>右上角</BeButton>
-    <BeButton size="normal" type="default" on:click={() =>openPosInfo('bottom-right')}>右下角</BeButton>
-    <BeButton size="normal" type="default" on:click={() =>openPosInfo('top-left')}>左上角</BeButton>
-    <BeButton size="normal" type="default" on:click={() =>openPosInfo('bottom-left')}>左下角</BeButton>
-  </div>
-  <h3>自定义颜色</h3>
-  <p>添加参数<code>titleColor</code>可修改标题颜色,添加<code>messageColor</code>可修改信息颜色</p>
-  <div>
-    <BeButton size="normal" type="default" on:click={openColorNotice}>自定义颜色</BeButton>
-  </div>
-  <h3>手动关闭</h3>
-  <p>目前手动关闭只能关闭最后一个notice使用<code>close</code>如果要一次性关闭所有可使用<code>closeAll</code>
-  <div>
-    <BeButton size="normal" type="default" on:click={openNotice}>弹出</BeButton>
-    <BeButton size="normal" type="default" on:click={closeOne}>关闭</BeButton>
-    <BeButton size="normal" type="default" on:click={closeAll}>关闭所有</BeButton>
-  </div>
-  <h3>监听notice关闭</h3>
-  <p>添加参数<code>onClose</code>类型为<code>Function</code></p>
-  <div>
-    <BeButton size="normal" type="default" on:click={openNotice1}>弹出</BeButton>
-  </div>
+	<div class=' demo-notice'>
+		<BeButton size='normal' type='default' on:click={openInfo}>点击弹出消息</BeButton>
+	</div>
+	<h3>指定位置</h3>
+	<p>默认为<code>top-right</code>可选择<code>bottom-right</code><code>top-left</code><code>bottom-left</code></p>
+	<div class=' demo-notice'>
+		<BeButton size='normal' type='default' on:click={() =>openPosInfo('top-right')}>右上角</BeButton>
+		<BeButton size='normal' type='default' on:click={() =>openPosInfo('bottom-right')}>右下角</BeButton>
+		<BeButton size='normal' type='default' on:click={() =>openPosInfo('top-left')}>左上角</BeButton>
+		<BeButton size='normal' type='default' on:click={() =>openPosInfo('bottom-left')}>左下角</BeButton>
+	</div>
+	<h3>自定义颜色</h3>
+	<p>添加参数<code>titleColor</code>可修改标题颜色,添加<code>messageColor</code>可修改信息颜色</p>
+	<div>
+		<BeButton size='normal' type='default' on:click={openColorNotice}>自定义颜色</BeButton>
+	</div>
+	<h3>手动关闭</h3>
+	<p>目前手动关闭只能关闭最后一个notice使用<code>close</code>如果要一次性关闭所有可使用<code>closeAll</code>
+	<div>
+		<BeButton size='normal' type='default' on:click={openNotice}>弹出</BeButton>
+		<BeButton size='normal' type='default' on:click={closeOne}>关闭</BeButton>
+		<BeButton size='normal' type='default' on:click={closeAll}>关闭所有</BeButton>
+	</div>
+	<h3>带有倾向性</h3>
+	<p>带有 icon，常用来显示「成功 <span>success</span>、警告 <span>warning</span>、消息 <span>info</span>、错误 <span>error</span>」类的系统消息</p>
+	<DemoBlock
+	js={`
+import Notice from "@brewer/beerui/utils/notice";
+let notice;
+onMount(() => {
+	notice = new Notice();
+});
+const openTypeNotice = (type) => {
+	notice.setNotice({
+		title: '提示',
+		message: type,
+		duration: 3000,
+		type
+	});
+}
+	`}
+	code={`
+	<BeButton size='normal' type='default' on:click={() => openTypeNotice('success')}>
+		<div style='display: flex'>
+			<BeIcon name='check-circle-filled' color='#67c23a' /> <span style='padding-left: 5px;'>成功</span>
+		</div>
+	</BeButton>
+	<BeButton size='normal' type='default' on:click={() => openTypeNotice('warning')}>
+		<div style='display: flex'>
+			<BeIcon name='error-circle-filled' color='#e6a23c' /> <span style='padding-left: 5px;'>警告</span>
+		</div>
+	</BeButton>
+	<BeButton size='normal' type='default' on:click={() => openTypeNotice('info')}>
+		<div style='display: flex'>
+			<BeIcon name='info-circle-filled' color='#909399' /> <span style='padding-left: 5px;'>消息</span>
+		</div>
+	</BeButton>
+	<BeButton size='normal' type='default' on:click={() => openTypeNotice('error')}>
+		<div style='display: flex'>
+			<BeIcon name='close-circle-filled' color='#f56c6c' /> <span style='padding-left: 5px;'>错误</span>
+		</div>
+	</BeButton>`}
+	>
+		<div slot='source'>
+			<BeButton size='normal' type='default' on:click={() => openTypeNotice('success')}>
+				<div style='display: flex'>
+					<BeIcon name='check-circle-filled' color='#67c23a' /> <span style='padding-left: 5px;'>成功</span>
+				</div>
+			</BeButton>
+			<BeButton size='normal' type='default' on:click={() => openTypeNotice('warning')}>
+				<div style='display: flex'>
+					<BeIcon name='error-circle-filled' color='#e6a23c' /> <span style='padding-left: 5px;'>警告</span>
+				</div>
+			</BeButton>
+			<BeButton size='normal' type='default' on:click={() => openTypeNotice('info')}>
+				<div style='display: flex'>
+					<BeIcon name='info-circle-filled' color='#909399' /> <span style='padding-left: 5px;'>消息</span>
+				</div>
+			</BeButton>
+			<BeButton size='normal' type='default' on:click={() => openTypeNotice('error')}>
+				<div style='display: flex'>
+					<BeIcon name='close-circle-filled' color='#f56c6c' /> <span style='padding-left: 5px;'>错误</span>
+				</div>
+			</BeButton>
+		</div>
+	</DemoBlock>
 
-  <h3>点击事件</h3>
-  <p>添加参数<code>onClick</code>类型为<code>Function</code></p>
-  <div>
-    <BeButton size="normal" type="default" on:click={openNotice2}>弹出</BeButton>
-  </div>
+	<h3>监听notice关闭</h3>
+	<p>添加参数<code>onClose</code>类型为<code>Function</code></p>
+	<div>
+		<BeButton size='normal' type='default' on:click={openNotice1}>弹出</BeButton>
+	</div>
+
+	<h3>点击事件</h3>
+	<p>添加参数<code>onClick</code>类型为<code>Function</code></p>
+	<div>
+		<BeButton size='normal' type='default' on:click={openNotice2}>弹出</BeButton>
+	</div>
 </div>
