b16209d32c238546cfb51e228aff3fc0d030950c
叶陈华 <742436967@qq.com>
2022-03-22 09:57:16 +0800

    feat：打包css

--- src/lib/assets/scss/dist/main.js
+++ src/lib/assets/scss/dist/main.js
@@ -7,6 +7,7 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
 /***/ "./index.js":
@@ -15,8 +16,7 @@
   \******************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-"use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _index_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./index.scss */ \"./index.scss\");\n/* harmony import */ var _index_scss__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_index_scss__WEBPACK_IMPORTED_MODULE_0__);\n\r\n\n\n//# sourceURL=webpack://scss/./index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _index_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./index.scss */ \"./index.scss\");\n\r\n\n\n//# sourceURL=webpack://scss/./index.js?");
 
 /***/ }),
 
@@ -24,9 +24,9 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _ind
 /*!********************!*\
   !*** ./index.scss ***!
   \********************/
-/***/ (() => {
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("throw new Error(\"Module build failed (from ./node_modules/mini-css-extract-plugin/dist/loader.js):\\nHookWebpackError: Module build failed (from ./node_modules/postcss-loader/dist/cjs.js):\\nError: PostCSS plugin postcss-discard-comments requires PostCSS 8.\\nMigration guide for end-users:\\nhttps://github.com/postcss/postcss/wiki/PostCSS-8-for-end-users\\n    at Processor.normalize (E:\\\\nginx-1.17.0\\\\BeerUi\\\\node_modules\\\\postcss\\\\lib\\\\processor.js:167:15)\\n    at new Processor (E:\\\\nginx-1.17.0\\\\BeerUi\\\\node_modules\\\\postcss\\\\lib\\\\processor.js:56:25)\\n    at postcss (E:\\\\nginx-1.17.0\\\\BeerUi\\\\node_modules\\\\postcss\\\\lib\\\\postcss.js:55:10)\\n    at cssnanoPlugin (E:\\\\nginx-1.17.0\\\\BeerUi\\\\node_modules\\\\cssnano\\\\dist\\\\index.js:158:31)\\n    at Object.<anonymous> (E:\\\\nginx-1.17.0\\\\BeerUi\\\\postcss.config.cjs:13:7)\\n    at Module._compile (node:internal/modules/cjs/loader:1101:14)\\n    at Object.Module._extensions..js (node:internal/modules/cjs/loader:1153:10)\\n    at Module.load (node:internal/modules/cjs/loader:981:32)\\n    at Function.Module._load (node:internal/modules/cjs/loader:822:12)\\n    at Module.require (node:internal/modules/cjs/loader:1005:19)\\n    at tryRunOrWebpackError (E:\\\\nginx-1.17.0\\\\BeerUi\\\\src\\\\lib\\\\assets\\\\scss\\\\node_modules\\\\webpack\\\\lib\\\\HookWebpackError.js:88:9)\\n    at __webpack_require_module__ (E:\\\\nginx-1.17.0\\\\BeerUi\\\\src\\\\lib\\\\assets\\\\scss\\\\node_modules\\\\webpack\\\\lib\\\\Compilation.js:4979:12)\\n    at __webpack_require__ (E:\\\\nginx-1.17.0\\\\BeerUi\\\\src\\\\lib\\\\assets\\\\scss\\\\node_modules\\\\webpack\\\\lib\\\\Compilation.js:4936:18)\\n    at E:\\\\nginx-1.17.0\\\\BeerUi\\\\src\\\\lib\\\\assets\\\\scss\\\\node_modules\\\\webpack\\\\lib\\\\Compilation.js:5007:20\\n    at symbolIterator (E:\\\\nginx-1.17.0\\\\BeerUi\\\\src\\\\lib\\\\assets\\\\scss\\\\node_modules\\\\neo-async\\\\async.js:3485:9)\\n    at done (E:\\\\nginx-1.17.0\\\\BeerUi\\\\src\\\\lib\\\\assets\\\\scss\\\\node_modules\\\\neo-async\\\\async.js:3527:9)\\n    at Hook.eval [as callAsync] (eval at create (E:\\\\nginx-1.17.0\\\\BeerUi\\\\src\\\\lib\\\\assets\\\\scss\\\\node_modules\\\\tapable\\\\lib\\\\HookCodeFactory.js:33:10), <anonymous>:15:1)\\n    at Hook.CALL_ASYNC_DELEGATE [as _callAsync] (E:\\\\nginx-1.17.0\\\\BeerUi\\\\src\\\\lib\\\\assets\\\\scss\\\\node_modules\\\\tapable\\\\lib\\\\Hook.js:18:14)\\n    at E:\\\\nginx-1.17.0\\\\BeerUi\\\\src\\\\lib\\\\assets\\\\scss\\\\node_modules\\\\webpack\\\\lib\\\\Compilation.js:4914:43\\n    at symbolIterator (E:\\\\nginx-1.17.0\\\\BeerUi\\\\src\\\\lib\\\\assets\\\\scss\\\\node_modules\\\\neo-async\\\\async.js:3482:9)\\n-- inner error --\\nError: Module build failed (from ./node_modules/postcss-loader/dist/cjs.js):\\nError: PostCSS plugin postcss-discard-comments requires PostCSS 8.\\nMigration guide for end-users:\\nhttps://github.com/postcss/postcss/wiki/PostCSS-8-for-end-users\\n    at Processor.normalize (E:\\\\nginx-1.17.0\\\\BeerUi\\\\node_modules\\\\postcss\\\\lib\\\\processor.js:167:15)\\n    at new Processor (E:\\\\nginx-1.17.0\\\\BeerUi\\\\node_modules\\\\postcss\\\\lib\\\\processor.js:56:25)\\n    at postcss (E:\\\\nginx-1.17.0\\\\BeerUi\\\\node_modules\\\\postcss\\\\lib\\\\postcss.js:55:10)\\n    at cssnanoPlugin (E:\\\\nginx-1.17.0\\\\BeerUi\\\\node_modules\\\\cssnano\\\\dist\\\\index.js:158:31)\\n    at Object.<anonymous> (E:\\\\nginx-1.17.0\\\\BeerUi\\\\postcss.config.cjs:13:7)\\n    at Module._compile (node:internal/modules/cjs/loader:1101:14)\\n    at Object.Module._extensions..js (node:internal/modules/cjs/loader:1153:10)\\n    at Module.load (node:internal/modules/cjs/loader:981:32)\\n    at Function.Module._load (node:internal/modules/cjs/loader:822:12)\\n    at Module.require (node:internal/modules/cjs/loader:1005:19)\\n    at Object.<anonymous> (E:\\\\nginx-1.17.0\\\\BeerUi\\\\src\\\\lib\\\\assets\\\\scss\\\\node_modules\\\\css-loader\\\\dist\\\\cjs.js??ruleSet[1].rules[1].use[1]!E:\\\\nginx-1.17.0\\\\BeerUi\\\\src\\\\lib\\\\assets\\\\scss\\\\node_modules\\\\postcss-loader\\\\dist\\\\cjs.js!E:\\\\nginx-1.17.0\\\\BeerUi\\\\src\\\\lib\\\\assets\\\\scss\\\\node_modules\\\\sass-loader\\\\dist\\\\cjs.js!E:\\\\nginx-1.17.0\\\\BeerUi\\\\src\\\\lib\\\\assets\\\\scss\\\\index.scss:1:7)\\n    at E:\\\\nginx-1.17.0\\\\BeerUi\\\\src\\\\lib\\\\assets\\\\scss\\\\node_modules\\\\webpack\\\\lib\\\\javascript\\\\JavascriptModulesPlugin.js:432:11\\n    at Hook.eval [as call] (eval at create (E:\\\\nginx-1.17.0\\\\BeerUi\\\\src\\\\lib\\\\assets\\\\scss\\\\node_modules\\\\tapable\\\\lib\\\\HookCodeFactory.js:19:10), <anonymous>:7:1)\\n    at Hook.CALL_DELEGATE [as _call] (E:\\\\nginx-1.17.0\\\\BeerUi\\\\src\\\\lib\\\\assets\\\\scss\\\\node_modules\\\\tapable\\\\lib\\\\Hook.js:14:14)\\n    at E:\\\\nginx-1.17.0\\\\BeerUi\\\\src\\\\lib\\\\assets\\\\scss\\\\node_modules\\\\webpack\\\\lib\\\\Compilation.js:4981:39\\n    at tryRunOrWebpackError (E:\\\\nginx-1.17.0\\\\BeerUi\\\\src\\\\lib\\\\assets\\\\scss\\\\node_modules\\\\webpack\\\\lib\\\\HookWebpackError.js:83:7)\\n    at __webpack_require_module__ (E:\\\\nginx-1.17.0\\\\BeerUi\\\\src\\\\lib\\\\assets\\\\scss\\\\node_modules\\\\webpack\\\\lib\\\\Compilation.js:4979:12)\\n    at __webpack_require__ (E:\\\\nginx-1.17.0\\\\BeerUi\\\\src\\\\lib\\\\assets\\\\scss\\\\node_modules\\\\webpack\\\\lib\\\\Compilation.js:4936:18)\\n    at E:\\\\nginx-1.17.0\\\\BeerUi\\\\src\\\\lib\\\\assets\\\\scss\\\\node_modules\\\\webpack\\\\lib\\\\Compilation.js:5007:20\\n    at symbolIterator (E:\\\\nginx-1.17.0\\\\BeerUi\\\\src\\\\lib\\\\assets\\\\scss\\\\node_modules\\\\neo-async\\\\async.js:3485:9)\\n\\nGenerated code for E:\\\\nginx-1.17.0\\\\BeerUi\\\\src\\\\lib\\\\assets\\\\scss\\\\node_modules\\\\css-loader\\\\dist\\\\cjs.js??ruleSet[1].rules[1].use[1]!E:\\\\nginx-1.17.0\\\\BeerUi\\\\src\\\\lib\\\\assets\\\\scss\\\\node_modules\\\\postcss-loader\\\\dist\\\\cjs.js!E:\\\\nginx-1.17.0\\\\BeerUi\\\\src\\\\lib\\\\assets\\\\scss\\\\node_modules\\\\sass-loader\\\\dist\\\\cjs.js!E:\\\\nginx-1.17.0\\\\BeerUi\\\\src\\\\lib\\\\assets\\\\scss\\\\index.scss\\n1 | throw new Error(\\\"Module build failed (from ./node_modules/postcss-loader/dist/cjs.js):\\\\nError: PostCSS plugin postcss-discard-comments requires PostCSS 8.\\\\nMigration guide for end-users:\\\\nhttps://github.com/postcss/postcss/wiki/PostCSS-8-for-end-users\\\\n    at Processor.normalize (E:\\\\\\\\nginx-1.17.0\\\\\\\\BeerUi\\\\\\\\node_modules\\\\\\\\postcss\\\\\\\\lib\\\\\\\\processor.js:167:15)\\\\n    at new Processor (E:\\\\\\\\nginx-1.17.0\\\\\\\\BeerUi\\\\\\\\node_modules\\\\\\\\postcss\\\\\\\\lib\\\\\\\\processor.js:56:25)\\\\n    at postcss (E:\\\\\\\\nginx-1.17.0\\\\\\\\BeerUi\\\\\\\\node_modules\\\\\\\\postcss\\\\\\\\lib\\\\\\\\postcss.js:55:10)\\\\n    at cssnanoPlugin (E:\\\\\\\\nginx-1.17.0\\\\\\\\BeerUi\\\\\\\\node_modules\\\\\\\\cssnano\\\\\\\\dist\\\\\\\\index.js:158:31)\\\\n    at Object.<anonymous> (E:\\\\\\\\nginx-1.17.0\\\\\\\\BeerUi\\\\\\\\postcss.config.cjs:13:7)\\\\n    at Module._compile (node:internal/modules/cjs/loader:1101:14)\\\\n    at Object.Module._extensions..js (node:internal/modules/cjs/loader:1153:10)\\\\n    at Module.load (node:internal/modules/cjs/loader:981:32)\\\\n    at Function.Module._load (node:internal/modules/cjs/loader:822:12)\\\\n    at Module.require (node:internal/modules/cjs/loader:1005:19)\\\");\");\n\n//# sourceURL=webpack://scss/./index.scss?");
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://scss/./index.scss?");
 
 /***/ })
 
@@ -57,35 +57,6 @@ eval("throw new Error(\"Module build failed (from ./node_modules/mini-css-extrac
 /******/ 	}
 /******/ 	
 /************************************************************************/
-/******/ 	/* webpack/runtime/compat get default export */
-/******/ 	(() => {
-/******/ 		// getDefaultExport function for compatibility with non-harmony modules
-/******/ 		__webpack_require__.n = (module) => {
-/******/ 			var getter = module && module.__esModule ?
-/******/ 				() => (module['default']) :
-/******/ 				() => (module);
-/******/ 			__webpack_require__.d(getter, { a: getter });
-/******/ 			return getter;
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/define property getters */
-/******/ 	(() => {
-/******/ 		// define getter functions for harmony exports
-/******/ 		__webpack_require__.d = (exports, definition) => {
-/******/ 			for(var key in definition) {
-/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
-/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
-/******/ 				}
-/******/ 			}
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
-/******/ 	(() => {
-/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
-/******/ 	})();
-/******/ 	
 /******/ 	/* webpack/runtime/make namespace object */
 /******/ 	(() => {
 /******/ 		// define __esModule on exports
