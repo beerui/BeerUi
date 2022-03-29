57f9ca29828227f540382687050a2e30ad926120
beerui <beerui2021@gmail.com>
2022-03-29 13:13:29 +0800

    fixed input type
    docs

--- src/lib/be-input/BeInput.svelte
+++ src/lib/be-input/BeInput.svelte
@@ -1,50 +1,63 @@
-<script lang="ts">
-  /**
-   * this is input component
-  */
-  import { createEventDispatcher } from 'svelte'
-  export let value = '';
-  export let placeholder = '';
-  export let size = '';
-  export let readonly = false;
-  export let disabled = false;
-  // 是否显示清除按钮
-  export let clearable = false;
-  export let type = 'text';
-  // 右侧icon
-  export let suffixIcon = ''
-  // let showClear = false;
-  let input
-  function showClear() {
-    return clearable&&!readonly&&!disabled
-  }
-  let suffix;
-  // 判断后置内容是否存在
-  function getSuffixVisible(){
-    return $$slots.suffix
-  }
+<script lang='ts'>
+	/**
+	 * this is input component
+	 */
+	import { createEventDispatcher } from 'svelte';
 
-  let dispatch = createEventDispatcher()
-  // 在input失去焦点时触发
-  function blur(event){
-    dispatch('blur',event)
-  }
-  // 是输入框获取焦点
-  export function getblur(){
-    input.focus()
-  }
-  // 在input获得焦点时触发
-  function focus(event){
-    dispatch('focus',event)
-  }
-  // 仅在输入框失去焦点或用户按下回车时触发
-  function change(event){
-    dispatch('change', event.target.value)
-  }
-  // 在 input 值改变时触发
-  function onInput(event){
-    dispatch('input', event.target.value)
-  }
+	export let value = '';
+	export let placeholder = '';
+	export let size = '';
+	export let readonly = false;
+	export let disabled = false;
+	// 是否显示清除按钮
+	export let clearable = false;
+	export let type = 'text';
+	// 右侧icon
+	export let suffixIcon = '';
+	// let showClear = false;
+	let input;
+
+	function showClear() {
+		return clearable && !readonly && !disabled;
+	}
+
+	let suffix;
+
+	// 判断后置内容是否存在
+	function getSuffixVisible() {
+		return $$slots.suffix;
+	}
+
+	let dispatch = createEventDispatcher();
+
+	// 在input失去焦点时触发
+	function blur(event) {
+		dispatch('blur', event);
+	}
+
+	// 是输入框获取焦点
+	export function getblur() {
+		input.focus();
+	}
+
+	// 在input获得焦点时触发
+	function focus(event) {
+		dispatch('focus', event);
+	}
+
+	// 仅在输入框失去焦点或用户按下回车时触发
+	function change(event) {
+		dispatch('change', event.target.value);
+	}
+
+	// 在 input 值改变时触发
+	function onInput(e) {
+		dispatch('input', e.target.value);
+	}
+
+	function typeAction(node) {
+		node.type = type;
+	}
 </script>
 
 <div
@@ -69,31 +82,31 @@
 	on:pointerup
 	on:input
 >
-  <input
-	  {type}
-     {placeholder}
-	  bind:value
-	  class="be-input__inner"
-	  {readonly}
-	  {disabled}
-	  on:blur={blur}
-	  on:focus={focus}
-	  on:change={change}
-	  on:input={onInput}
-	  bind:this={input}
-  >
-  {#if getSuffixVisible()}
-  <span class={['be-input__suffix',disabled ? ' is-disabled':''].join('')}>
-    <span class="be-input__suffix-inner ">
-      <template>
-        <slot name="suffix"></slot>
-        <!-- suffixIcon -->
-        {#if suffixIcon}
-        <i class={['be-input__icon ', suffixIcon].join('')}></i>
-        {/if}
-      </template>
-    </span>
-  </span>
-  {/if}
+	<input
+		use:typeAction
+		placeholder={placeholder}
+		bind:value
+		class='be-input__inner'
+		{readonly}
+		{disabled}
+		on:blur={blur}
+		on:focus={focus}
+		on:change={change}
+		on:input={onInput}
+		bind:this={input}
+	/>
+	{#if getSuffixVisible()}
+	  <span class={['be-input__suffix',disabled ? ' is-disabled':''].join('')}>
+	    <span class='be-input__suffix-inner '>
+	      <template>
+	        <slot name='suffix'></slot>
+		      <!-- suffixIcon -->
+		      {#if suffixIcon}
+	        <i class={['be-input__icon ', suffixIcon].join('')}></i>
+	        {/if}
+	      </template>
+	    </span>
+	  </span>
+	{/if}
 </div>
 
--- src/routes/components/input/index.svelte
+++ src/routes/components/input/index.svelte
@@ -1,46 +1,71 @@
 <script lang="ts">
     import BeInput from '$lib/be-input/BeInput.svelte'
     import { onMount } from 'svelte'
+    import DemoBlock from '$lib/demo/DemoBlock.svelte';
     let value = ''
     let input
     onMount(()=>{
       console.log(input)
     })
+    let number;
+    let password;
+    let email;
 </script>
 <div class="page-container content">
 	<h2>input 输入框</h2>
 	<p>通过鼠标或键盘输入字符</p>
 	<h3>基本用法</h3>
-	<div class="demo-block demo-input ">
-		<div class="source">
-			<div>
+
+	<ol>
+		<li>checked 绑定选中的值</li>
+		<li>inactiveColor 选中颜色</li>
+		<li>change 事件监听</li>
+	</ol>
+	<h3>尺寸</h3>
+	<p>可通过 size 属性指定输入框的尺寸，除了默认的大小外，还提供了<code>medium</code> 、<code>small</code> 和<code>mini</code>  三种尺寸。</p>
+	<DemoBlock code={`
+<BeInput bind:value placeholder="请输入内容" />
+<BeInput bind:value placeholder="请输入内容"  size="medium" />
+<BeInput bind:value placeholder="请输入内容"  size="small" />
+<BeInput bind:value placeholder="请输入内容"  size="mini" />
+`} js={`
+import { BeInput } from '@brewer/beerui';
+let value = ''
+`}>
+		<div slot="source">
+			<div class="demo-list">
 				<BeInput bind:value placeholder="请输入内容" />
+				<hr>
+				<BeInput bind:value placeholder="请输入内容" size="medium" />
+				<hr>
+				<BeInput bind:value placeholder="请输入内容" size="small" />
+				<hr>
+				<BeInput bind:value placeholder="请输入内容" size="mini" />
 			</div>
 		</div>
-	</div>
-    <h3>尺寸</h3>
-    <p>可通过 size 属性指定输入框的尺寸，除了默认的大小外，还提供了<code>medium</code> 、<code>small</code> 和<code>mini</code>  三种尺寸。</p>
-	<div class="demo-block demo-input ">
-		<div class="source">
-			<div>
-        <div class="demo-input-size">
-            <BeInput bind:value placeholder="请输入内容" bind:this={input} />
-            <BeInput bind:value placeholder="请输入内容"  size="medium" />
-            <BeInput bind:value placeholder="请输入内容"  size="small" />
-            <BeInput bind:value placeholder="请输入内容"  size="mini" />
-        </div>
+	</DemoBlock>
+	<h3>禁用状态</h3>
+	<p>通过<code>disabled</code> 属性指定是否禁用 input 组件</p>
+	<DemoBlock code={`
+<BeInput placeholder="请输入内容" disabled />
+`}>
+		<div slot="source">
+			<div class="demo-list">
+				<BeInput bind:value placeholder="请输入内容" disabled />
 			</div>
 		</div>
-	</div>
-  <h3>禁用状态</h3>
-  <p>通过<code>disabled</code> 属性指定是否禁用 input 组件</p>
-<div class="demo-block demo-input ">
-  <div class="source">
-    <div>
-      <div class="demo-input-size">
-          <BeInput bind:value placeholder="请输入内容" disabled />
-      </div>
-    </div>
-  </div>
-</div>
+	</DemoBlock>
+	<h3>类型</h3>
+	<p>通过<code>type</code> 属性指定</p>
+	<DemoBlock code={`
+<BeInput placeholder="请输入内容" disabled />
+`}>
+		<div slot="source">
+			<div class="demo-list">
+				number：{number}<BeInput bind:value={number} type='number' placeholder="请输入数字" />
+				<hr>
+				password：{password}<BeInput bind:value={password} type='password' placeholder="请输入密码" />
+			</div>
+		</div>
+	</DemoBlock>
 </div>
