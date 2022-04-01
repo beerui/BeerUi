e7ec25093448ebc31f2ba0081b55c2e22e8f900b
beerui <beerui2021@gmail.com>
2022-04-01 08:49:47 +0800

    docs

--- src/routes/components/preview/index.svelte
+++ src/routes/components/preview/index.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { previewImages } from '$lib';
+  import DemoBlock from '$lib/demo/DemoBlock.svelte';
   const src = '/images/1.jpg'
 	const list = ['/images/1.jpg', '/images/2.jpg', '/images/3.jpg', '/images/4.jpg', '/images/5.jpg', '/images/6.jpg']
   const clickImage = (src: string) => {
@@ -15,23 +16,39 @@
 </script>
 <div class='page-container'>
 	<h2>previewImages 预览图片</h2>
-	<p>
-		未来会添加的功能
-		1、放大
-		2、缩小
-		3、旋转
-		4、全屏
-	</p>
-	<h3>预览多图</h3>
-	<div class='demo-list'>
-		{#each list as item}
-		<img class='demo-list-image' on:click={() => {clickImage(item)}} src={item} alt=''>
-		{/each}
-	</div>
+	<h3>基础用法</h3>
+	<p>预览多图</p>
+	<DemoBlock code={`
+{#each list as item}
+	<img class='demo-list-image' on:click={() => {clickImage(item)}} src={item} alt=''>
+{/each}
+`} js={`
+import { previewImages } from '@brewer/beerui'
+const list = ['/images/1.jpg', '/images/2.jpg', '/images/3.jpg', '/images/4.jpg', '/images/5.jpg', '/images/6.jpg']
+const clickImage = (src: string) => {
+	const p1 = previewImages(src, list)
+	// 调用方法 监听图片的改变
+	p1.watchHandle(res => console.log(res))
+}`}>
+		<div slot="source">
+			<div class='demo-list'>
+				{#each list as item}
+					<img class='demo-list-image' on:click={() => {clickImage(item)}} src={item} alt=''>
+				{/each}
+			</div>
+		</div>
+	</DemoBlock>
 	<h3>预览一张图</h3>
-	<div class='demo-list'>
-		<img class='demo-list-image' on:click={() => {previewImages('/images/7.jpg')}} src='/images/7.jpg' alt=''>
-	</div>
+	<DemoBlock code={`
+<img class='demo-list-image' on:click={() => {previewImages('/images/7.jpg')}} src='/images/7.jpg' alt=''>`} js={`
+import { previewImages } from '@brewer/beerui'
+`}>
+		<div slot="source">
+			<div class='demo-list'>
+				<img class='demo-list-image' on:click={() => {previewImages('/images/7.jpg')}} src='/images/7.jpg' alt=''>
+			</div>
+		</div>
+	</DemoBlock>
 </div>
 <style lang='scss'>
 	.demo-list {
