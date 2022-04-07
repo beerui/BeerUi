7449f87400dd6dc2d45fd2907c57f493fa4d2845
叶陈华 <742436967@qq.com>
2022-04-07 11:06:36 +0800

    FIXED：BeCountTo 暴露开始、暂停、重置方法

--- src/routes/components/countTo/index.svelte
+++ src/routes/components/countTo/index.svelte
@@ -12,7 +12,11 @@
 
 <div class='page-container'>
   <DemoBlock code = {`
-     <BeCountTo start={start} end={end} duration={duration} />
+     <BeCountTo {start} {end} {duration} bind:this={countTo}/>
+     <BeButton on:click={countTo.startHandler()}>开始</BeButton>
+    <BeButton on:click={countTo.pauseHandler()}>暂停</BeButton>
+    <BeButton on:click={countTo.resumeHandler()}>恢复</BeButton>
+    <BeButton on:click={countTo.resetHandler()}>重置</BeButton>
     `} js={` 
     let countTo
     let start = 0
@@ -21,14 +25,14 @@
     let decimal = 0`}>
 		<div slot='source'>
       <div class="count-to-example-num">
-        <BeCountTo start={start} end={end} duration={duration} bind:this={countTo} autoplay={false}/>
+        <BeCountTo {start} {end} {duration} bind:this={countTo} />
       </div>
       
       <div class="count-to-example">
-        <div class="count-to-example-input">start： <BeInput bind:value={start}/></div>
-        <div class="count-to-example-input">end： <BeInput bind:value={end}/></div>
-        <div class="count-to-example-input">duration： <BeInput bind:value={duration}/></div>
-        <div class="count-to-example-input">decimal： <BeInput bind:value={decimal}/></div>
+        <div class="count-to-example-input">start： <BeInput type="number" bind:value={start}/></div>
+        <div class="count-to-example-input">end： <BeInput type="number" bind:value={end}/></div>
+        <div class="count-to-example-input">duration： <BeInput type="number" bind:value={duration}/></div>
+        <div class="count-to-example-input">decimals： <BeInput type="number" bind:value={decimal}/></div>
       </div>
       <div class="count-to-example">
         <BeButton style="margin-right:10px" on:click={countTo.startHandler()}>开始</BeButton>
@@ -38,7 +42,13 @@
       </div>
 		</div>
 		<div slot='description'>
-      
+      <p>start: 开始数值</p>
+      <p>end: 结束数值</p>
+      <p>durtaion: 持续时间</p>
+      <p>decimals: 保留小数点后几位</p>
+      <p>autoplay: 是否自动累加</p>
+      <p>useEasing: 动画是否以低速结束</p>
+      <p>separator: 分隔符</p>
 		</div>
 	</DemoBlock>
 </div>
