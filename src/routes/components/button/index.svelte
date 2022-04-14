8903e3831102df7e6af9bfd7e17f34801ee83fe5
jyc <1007910819@qq.com>
2022-04-14 15:49:59 +0800

    chore sass variables change

--- src/routes/components/button/index.svelte
+++ src/routes/components/button/index.svelte
@@ -3,101 +3,134 @@
   import DemoBlock from '$lib/demo/DemoBlock.svelte';
   import BeIcon from '$lib/be-icon/BeIcon.svelte';
 
-  let js = `import { BeButton } from '@brewer/beerui'`
-  let code = `
+  const code = `
 <div class='demo-list'>
-	<BeButton size="normal" type="default">默认按钮</BeButton>
-	<BeButton size="normal" type="primary">主要按钮</BeButton>
-	<BeButton size="normal" type="success">成功按钮</BeButton>
-	<BeButton size="normal" type="info">信息按钮</BeButton>
-	<BeButton size="normal" type="warning">警告按钮</BeButton>
-	<BeButton size="normal" type="danger">危险按钮</BeButton>
+	<BeButton class='demo-button' type="default">默认按钮</BeButton>
+	<BeButton class='demo-button' type="primary">主要按钮</BeButton>
+	<BeButton class='demo-button' type="success">成功按钮</BeButton>
+	<BeButton class='demo-button' type="info">信息按钮</BeButton>
+	<BeButton class='demo-button' type="warning">警告按钮</BeButton>
+	<BeButton class='demo-button' type="danger">危险按钮</BeButton>
 </div>
 <div class='demo-list'>
-	<BeButton size="normal" round='lg' type="default">圆角按钮</BeButton>
-	<BeButton size="normal" round='lg' type="primary">主要按钮</BeButton>
-	<BeButton size="normal" round='lg' type="success">成功按钮</BeButton>
-	<BeButton size="normal" round='lg' type="info">信息按钮</BeButton>
-	<BeButton size="normal" round='lg' type="warning">警告按钮</BeButton>
-	<BeButton size="normal" round='lg' type="danger">危险按钮</BeButton>
+	<BeButton class='demo-button' type="default" round>圆角按钮</BeButton>
+	<BeButton class='demo-button' type="primary">主要按钮</BeButton>
+	<BeButton class='demo-button' type="success">成功按钮</BeButton>
+	<BeButton class='demo-button' type="info">信息按钮</BeButton>
+	<BeButton class='demo-button' type="warning">警告按钮</BeButton>
+	<BeButton class='demo-button' type="danger">危险按钮</BeButton>
 </div>
-<BeButton class='demo-button' disabled size="normal" round='lg' type="default">圆角按钮</BeButton>
-<BeButton class='demo-button' disabled size="normal" round='lg' type="primary">主要按钮</BeButton>
-<BeButton class='demo-button' disabled size="normal" round='lg' type="success">成功按钮</BeButton>
-<BeButton class='demo-button' disabled size="normal" round='lg' type="info">信息按钮</BeButton>
-<BeButton class='demo-button' disabled size="normal" round='lg' type="warning">警告按钮</BeButton>
-<BeButton class='demo-button' disabled size="normal" round='lg' type="danger">危险按钮</BeButton>
-<BeButton class='demo-button' size="normal" round='circle' icon='IconActivity' type="default">
+<BeButton class='demo-button' disabled type="default" round>圆角按钮</BeButton>
+<BeButton class='demo-button' disabled type="primary" round>主要按钮</BeButton>
+<BeButton class='demo-button' disabled type="success" round>成功按钮</BeButton>
+<BeButton class='demo-button' disabled type="info" round>信息按钮</BeButton>
+<BeButton class='demo-button' disabled type="warning" round>警告按钮</BeButton>
+<BeButton class='demo-button' disabled type="danger" round>危险按钮</BeButton>
+<BeButton class='demo-button' icon='IconActivity' type="default" circle>
 	<BeIcon name='add' color='#000' />
 </BeButton>
-<BeButton class='demo-button' size="normal" round='circle' type="primary">
+<BeButton class='demo-button' type="primary" circle>
 	<BeIcon name='books' color='#fff' />
 </BeButton>
-<BeButton class='demo-button' size="normal" round='circle' type="success">
+<BeButton class='demo-button' type="success" circle>
 	<BeIcon name='call' color='#fff' />
 </BeButton>
-<BeButton class='demo-button' size="normal" round='circle' type="info">
+<BeButton class='demo-button' type="info" circle>
 	<BeIcon name='chat' color='#fff' />
 </BeButton>
-<BeButton class='demo-button' size="normal" round='circle' type="warning">
+<BeButton class='demo-button' type="warning" circle>
 	<BeIcon name='clear' color='#fff' />
 </BeButton>
-<BeButton class='demo-button' size="normal" round='circle' type="danger">
+<BeButton class='demo-button' type="danger" circle>
 	<BeIcon name='cloud' color='#fff' />
 </BeButton>
+`
+	const code2 = `
+<div class='demo-list'>
+	<BeButton class='demo-button' type="default">默认按钮</BeButton>
+	<BeButton class='demo-button' type="default" size="medium">主要按钮</BeButton>
+	<BeButton class='demo-button' type="default" size="small">成功按钮</BeButton>
+	<BeButton class='demo-button' type="default" size="mini">信息按钮</BeButton>
+</div>
+<div class='demo-list'>
+	<BeButton class='demo-button' type="default" round>圆角按钮</BeButton>
+	<BeButton class='demo-button' type="default" size="medium" round>圆角按钮</BeButton>
+	<BeButton class='demo-button' type="default" size="small" round>圆角按钮</BeButton>
+	<BeButton class='demo-button' type="default" size="mini" round>圆角按钮</BeButton>
+</div>
 `
 </script>
 <div class='page-container'>
 	<h2>BeButton 按钮</h2>
-	<DemoBlock {code} {js}>
+	<DemoBlock {code} js={`import { BeButton } from '@brewer/beerui`}>
 		<div slot='source'>
 			<div class='demo-list'>
-				<BeButton class='demo-button' size="normal" type="default">默认按钮</BeButton>
-				<BeButton class='demo-button' size="normal" type="primary">主要按钮</BeButton>
-				<BeButton class='demo-button' size="normal" type="success">成功按钮</BeButton>
-				<BeButton class='demo-button' size="normal" type="info">信息按钮</BeButton>
-				<BeButton class='demo-button' size="normal" type="warning">警告按钮</BeButton>
-				<BeButton class='demo-button' size="normal" type="danger">危险按钮</BeButton>
+				<BeButton class='demo-button' type="default">默认按钮</BeButton>
+				<BeButton class='demo-button' type="primary">主要按钮</BeButton>
+				<BeButton class='demo-button' type="success">成功按钮</BeButton>
+				<BeButton class='demo-button' type="info">信息按钮</BeButton>
+				<BeButton class='demo-button' type="warning">警告按钮</BeButton>
+				<BeButton class='demo-button' type="danger">危险按钮</BeButton>
 			</div>
 			<div class='demo-list'>
-				<BeButton class='demo-button' size="normal" round='lg' type="default">圆角按钮</BeButton>
-				<BeButton class='demo-button' size="normal" round='lg' type="primary">主要按钮</BeButton>
-				<BeButton class='demo-button' size="normal" round='lg' type="success">成功按钮</BeButton>
-				<BeButton class='demo-button' size="normal" round='lg' type="info">信息按钮</BeButton>
-				<BeButton class='demo-button' size="normal" round='lg' type="warning">警告按钮</BeButton>
-				<BeButton class='demo-button' size="normal" round='lg' type="danger">危险按钮</BeButton>
+				<BeButton class='demo-button' type="default" round>圆角按钮</BeButton>
+				<BeButton class='demo-button' type="primary" round>主要按钮</BeButton>
+				<BeButton class='demo-button' type="success" round>成功按钮</BeButton>
+				<BeButton class='demo-button' type="info" round>信息按钮</BeButton>
+				<BeButton class='demo-button' type="warning" round>警告按钮</BeButton>
+				<BeButton class='demo-button' type="danger" round>危险按钮</BeButton>
 			</div>
 			<div class='demo-list'>
-				<BeButton class='demo-button' disabled size="normal" round='lg' type="default">圆角按钮</BeButton>
-				<BeButton class='demo-button' disabled size="normal" round='lg' type="primary">主要按钮</BeButton>
-				<BeButton class='demo-button' disabled size="normal" round='lg' type="success">成功按钮</BeButton>
-				<BeButton class='demo-button' disabled size="normal" round='lg' type="info">信息按钮</BeButton>
-				<BeButton class='demo-button' disabled size="normal" round='lg' type="warning">警告按钮</BeButton>
-				<BeButton class='demo-button' disabled size="normal" round='lg' type="danger">危险按钮</BeButton>
+				<BeButton class='demo-button' disabled type="default" round>圆角按钮</BeButton>
+				<BeButton class='demo-button' disabled type="primary">主要按钮</BeButton>
+				<BeButton class='demo-button' disabled type="success">成功按钮</BeButton>
+				<BeButton class='demo-button' disabled type="info">信息按钮</BeButton>
+				<BeButton class='demo-button' disabled type="warning">警告按钮</BeButton>
+				<BeButton class='demo-button' disabled type="danger">危险按钮</BeButton>
 			</div>
 			<div class='demo-list'>
-				<BeButton class='demo-button' size="normal" round='circle' icon='IconActivity' type="default">
+				<BeButton class='demo-button' icon='IconActivity' type="default" circle>
 					<BeIcon name='add' color='#000' />
 				</BeButton>
-				<BeButton class='demo-button' size="normal" round='circle' type="primary">
+				<BeButton class='demo-button' type="primary" circle>
 					<BeIcon name='books' color='#fff' />
 				</BeButton>
-				<BeButton class='demo-button' size="normal" round='circle' type="success">
+				<BeButton class='demo-button' type="success" circle>
 					<BeIcon name='call' color='#fff' />
 				</BeButton>
-				<BeButton class='demo-button' size="normal" round='circle' type="info">
+				<BeButton class='demo-button' type="info" circle>
 					<BeIcon name='chat' color='#fff' />
 				</BeButton>
-				<BeButton class='demo-button' size="normal" round='circle' type="warning">
+				<BeButton class='demo-button' type="warning" circle>
 					<BeIcon name='clear' color='#fff' />
 				</BeButton>
-				<BeButton class='demo-button' size="normal" round='circle' type="danger">
+				<BeButton class='demo-button' type="danger" circle>
 					<BeIcon name='cloud' color='#fff' />
 				</BeButton>
 			</div>
 		</div>
 		<div slot='description'>
 			使用type、plain、round、disabled和circle属性来定义 Button 的样式。
+		</div>
+	</DemoBlock>
+	<h2>不同尺寸</h2>
+	<DemoBlock code={code2}>
+		<div slot='source'>
+			<div class='demo-list'>
+				<BeButton class='demo-button' type="default">默认按钮</BeButton>
+				<BeButton class='demo-button' type="default" size="medium">主要按钮</BeButton>
+				<BeButton class='demo-button' type="default" size="small">成功按钮</BeButton>
+				<BeButton class='demo-button' type="default" size="mini">信息按钮</BeButton>
+			</div>
+			<div class='demo-list'>
+				<BeButton class='demo-button' type="default" round>圆角按钮</BeButton>
+				<BeButton class='demo-button' type="default" size="medium" round>圆角按钮</BeButton>
+				<BeButton class='demo-button' type="default" size="small" round>圆角按钮</BeButton>
+				<BeButton class='demo-button' type="default" size="mini" round>圆角按钮</BeButton>
+			</div>
+		</div>
+		<div slot='description'>
+			额外的尺寸：medium、small、mini，通过设置size属性来配置它们。
 		</div>
 	</DemoBlock>
 </div>
