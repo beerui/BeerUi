c16376d4f88be120f678a2de87c6fead775f4053
叶陈华 <742436967@qq.com>
2022-05-11 16:41:42 +0800

    ADD：switch增加切换前的回调

--- src/lib/be-switch/BeSwitch.svelte
+++ src/lib/be-switch/BeSwitch.svelte
@@ -1,22 +1,18 @@
 <script lang="ts">
 
 	import { onMount, createEventDispatcher } from 'svelte';
-
   const dispatch = createEventDispatcher()
-
   export let checked:boolean = false
-
   export let disabled:boolean = false
-
   export let width:number = 40
-
   export let activeColor:string
-
   export let inactiveColor:string
-
+  export let beforeChange = () =>{ return true}
   let switchColor = ''
   function switchValue() {
-    !switchDisabled && handleChange();
+    if(beforeChange()) {
+      !switchDisabled && handleChange();
+    }
   }
 
   function setBackgroundColor(check) {
