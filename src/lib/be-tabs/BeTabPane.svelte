019702ac8b927c4b93fedae0f1681427b257ae93
beerui <beerui2021@gmail.com>
2022-05-23 10:49:52 +0800

    fixed remove overflow

--- src/lib/be-tabs/BeTabPane.svelte
+++ src/lib/be-tabs/BeTabPane.svelte
@@ -3,12 +3,11 @@ export let active:string
 export let key:string
 $: isActive = (key === active)
 </script>
-<div class='be-tabs__content hidden' class:block={isActive}>
+<div class='be-tabs__content' class:hidden={!isActive} class:block={isActive}>
 	<slot></slot>
 </div>
-
 <style lang="scss">
-  .be-tabs__content {overflow: hidden;position: relative;}
+  .be-tabs__content {position: relative;}
   .hidden {display: none;}
   .block {display: block;}
 </style>
