9bff917ac9615e4dc09275a2d8b2ea9e2741d11a
liuyongjie <liuyongjie@anthead.cn>
2022-12-15 17:12:33 +0800

    FIXED 显示bug ADD 新增一些功能

---
+++ src/routes/(level1)/+layout.svelte
@@ -0,0 +1,21 @@
+<script>
+	import  '$lib/assets/beer.css'
+	import LayoutHeader from '$lib/demo/layout/LayoutHeader.svelte';
+	import { page } from '$app/stores';
+	import LayoutFooter from '$lib/demo/layout/LayoutFooter.svelte';
+	import {preparePageTransition} from '$lib/page-transition.js';
+
+	preparePageTransition();
+</script>
+<div class='be-warp'>
+	<LayoutHeader menu={$page.url.pathname} />
+	<div class='be-container'>
+		<slot></slot>
+		<LayoutFooter />
+	</div>
+</div>
+<style>
+    .be-warp {
+        background: rgb(107, 132, 173);
+    }
+</style>
